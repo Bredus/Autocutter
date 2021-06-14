cd35aa477c4c22116203caf75a41f0d46869032c
Oliver Bentham <72699889+Bredus@users.noreply.github.com>
2021-06-14 18:29:15 +0100

    Update
     + Timestamps allow for '.' between minutes and seconds - e.g. 2.30 for 2 min & 30 seconds
     + Prompts for source media and timestamp file
     + Checks timestamps exist within source media's time range
     + All new items are stored in a 'AutoCutter' bin to avoid conflicting names with existing project items
     + Allows for import of most video formats compatible with Adobe PP - and possibly audio formats too
     + No longer need to set preset for sequence - based on source media
     + More warning alerts
     + General code improvements & bug fixes

--- autocutter.jsx
+++ autocutter.jsx
@@ -1,131 +1,91 @@
-alert(main() == 0 ? "Complete!":"Action Failed.");
+// Root project bin
+var root = app.project.rootItem;
+// Autocutter/SourceVideo/Subclips Bins
+var acBin;
+var sourceBin;
+var subclipsBin;
+// Timestamps from txt file
+var timestamps = [];
+// Subclip items
+var subclips = [];
+// Maximum timestamp & line of .txt it was recorded on
+var maxTS = {
+    time: 0,
+    line: 0
+};
+
+if (main() == 0)
+{
+    alert("Complete");
+}
+else
+{
+    alert("AutoCutter Ended");
+    if (acBin != null)
+    {
+        acBin.deleteBin();
+    }
+
+}
 
 function main()
 {
-    // REGISTER BINS & FILES
-    var root = app.project.rootItem;
+    // CREATE Bins
+    acBin = newBin(root, "AutoCutter");
+    sourceBin = acBin.createBin("source");
+    subclipsBin = acBin.createBin("subclips");
 
-    // IMPORT SOURCE VIDEO
-    if (importVideo() != 0)
+
+    // FETCH source video file
+    var sourceFile = fetchFile("video formats:*.3gp;*.avi;*.m2ts;*.mts;*.mp4;*.m4v;*.mov;*.mp4;*.m4v;*.mod;*.mpeg;*.m2v;*.mpg;*.m2t;*.mov;*.m4a;*.vob;*.wmv,audio formats:*.amr;*.aif;*.aiff;*.aac;*.ac3;*.mp3;*.mov;*.wav;*.wma"
+    , "Select Source Media");
+    if (sourceFile == 1) return 1;
+
+    // FETCH timestamp text file
+    var tsFile = fetchFile("*.txt", "Select Timestamp file");
+    if (tsFile == 1) return 1;
+
+    
+    // LOAD Timestamps from file
+    if (loadTimestamps(tsFile) == 1)
+    {
+        return 1;
+    }
+
+    // IMPORT Source Video
+    if (importVideo(sourceFile) == 1)
     {
         alert("Video Import Failed");
         return 1;
     }
 
-    
-    // LOAD TIMESTAMP FILE
-    var timestamps = [];
-
-    // Load timestamps from textfile
-    if (loadTimestamps(timestamps) == 1)
-    {
-        alert("Failed to Load Timestamps");
-        return 1;
-    }
-
-    // Convert timestamps to seconds
-    if (convertTimestamps(timestamps) == 1)
-    {
-        alert("Failed to Convert Timestamps");
-        return 1;
-    }
-
-
-    // CREATE NEW SEQUENCE
-    var seq = createSequence()
-    if (seq == 1)
-    {
-        alert("Failed to Create Sequence");
-        return 1;
-    }
-
-
-    // CREATE SUBCLIPS,
-    var subclips = [];
-    if (createSubclips(timestamps, subclips) == 1)
+    // CREATE Subclips from Source Video
+    if (createSubclips(subclips) == 1)
     {
         alert("Failed to Create Subclips");
         return 1;
     }
     
+    // COMBINE Clips into a Sequence
+    app.project.createNewSequenceFromClips("new sequence", subclips, acBin);
 
-    //ADD CLIPS TO SEQUENCE    
-    var seq = findItem("seq00", root);
-    if (addClipsToSequence(subclips, seq.name) == 1)
-    {
-        alert("Failed to Add Clips to Sequence");
-        return 1;
-    }
-
-
-    return 0;
-}
-
-
-function importVideo()
-{
-    alert("Select Video to cut");
-
-    var file = File.openDialog("Select Source Video", "*.mp4");
-
-    if (file == null)
-    {
-        alert("file not selected");
-        return 1;
-    }
-
-    var sourceBin = findItem("source", app.project.rootItem);
-
-    if (sourceBin == null)
-    {
-        sourceBin = app.project.rootItem.createBin("source");
-    }
-
-
-    if (findItem(file.displayName, sourceBin) != null)
-    {
-        if (confirm("File already exists. Do you want to cut this file?",false))
-        {
-            return 0;
-        }
-    }
-
-    app.project.importFiles(file.fsName, 0, sourceBin, 0);
-    
     return 0;
 }
 
 
 // LOADS Timestamps to Array
-function loadTimestamps(arr)
+function loadTimestamps(file)
 {
-    alert("Select Timestamp .txt file");
-    var file = File.openDialog("Select Text File", "*.txt");
-
-    // ERROR CHECK - is file NULL
-    if (file == null)
-    {
-        alert("Text file not selected");
-        return 1;
-    }
-
-    var path = file.fsName;
-
-    // ERROR CHECK - is file a .txt
-    if (path.substring(path.length-4, path.length) != ".txt")
-    {
-        log("timestamp filetype is not a .txt!");
-        return 1;
-    }
-
     //READ FILE
-    file.open('r');
     var count = 0; // number of timestamps loaded
     var line = 1; // line number of text file - starting from 1
+    file.open('r');
+
     while(!file.eof)
     {
+        var ts = [];
         var currentline = file.readln();
-        currentline = currentline.replace(" ", "");
+        currentline = currentline.replace(/\s/g, "");
 
         if (currentline == "")
         {
@@ -133,195 +93,141 @@ function loadTimestamps(arr)
             continue;
         }
 
-        // ERROR CHECK - each line should be [start time]-[end time] where [*] are timestamps
+        // ERROR CHECK - each line should be [start time]-[end time]
         if (currentline.split('-').length != 2)
         {
-            log("Text file formatting error[1] on line " + line);
             file.close();
+            alert("Timestamp Error on line " + line);
             return 1;
         }
 
-        arr.push(currentline.split('-'));
-        arr[count].push(line);
+        ts = currentline.split('-');
+        //arr.push(currentline.split('-'));
+
+        // Convert timestamp to seconds
+        ts[0] = convertToSec(ts[0]);
+        ts[1] = convertToSec(ts[1]);
+
+        // ERROR - unable to convert to seconds
+        if (ts[0] == -1 || ts[1] == -1)
+        {
+            file.close();
+            alert("Timestamp Error on line " + line);
+            return 1;
+        }
+
+        // ERROR CHECK - start must be less than end
+        if (ts[0] >= ts[1])
+        {
+            file.close();
+            alert("Timestamp Error on line " + line);
+            return 1;
+        }
+
+        // UPDATE max timestamp
+        if (ts[1] > maxTS.time)
+        {
+            maxTS.time = ts[1];
+            maxTS.line = line;
+        }
+
+        timestamps.push(ts);
 
         line++;
         count++;
     }
+
     file.close();
 
-    if (arr.length == 0)
+    // ERROR - text file must contain timestamps
+    if (timestamps.length == 0)
     {
-        log("Error: No Timestamps Loaded");
+        alert("Error: No timestamps found in .txt file");
         return 1;
     }
 
     return 0;
 }
 
-// CONVERTS timestamps to SECONDS
-function convertTimestamps(arr)
+// CONVERTS timestamp to SECONDS
+function convertToSec(timestamp)
 {
-    // LOOP through each timestamp pair
-    for (var i = 0; i < arr.length; i++)
+    var sep = "";
+
+    // finding seperator ":" or "."
+    if (timestamp.split(":").length == 2)
+        sep = ":";
+    else if (timestamp.split(".").length == 2)
+        sep = ".";
+    else // ERROR CHECK - must contain one ":" or "."
+        return -1;
+
+    var min = timestamp.split(sep)[0];
+    var sec = timestamp.split(sep)[1];
+
+    // ERROR CHECK - must be integers
+    if (!isInt(min) || !isInt(sec) )
     {
-        var start = arr[i][0].split(':');
-        var end = arr[i][1].split(':');
+        return -1;
+    }
 
-        // ERROR CHECK - has one ':'
-        if (start.length != 2 || end.length != 2)
-        {
-            log("Text file formatting error[2] on line " + (arr[i][2]));
-            return 1;
-        }
+    // ERROR CHECK - seconds must be less than 60
+    if (sec >= 60)
+    {
+        return -1;
+    }
 
-        // ERROR CHECK - must be integers
-        if (!isInt(start[0]) || !isInt(start[1])
-         || !isInt(end[0]) || !isInt(end[1]) )
-        {
-            log("Text file formatting error[3] on line " + (arr[i][2]));
-            return 1;
-        }
+    return parseInt(min) * 60 + parseInt(sec);
+}
 
-        // ERROR CHECK - seconds must be less than 60
-        if (start[1] >= 60 || end[1] >= 60)
-        {
-            log("Text file formatting error[4] on line " + (arr[i][2]));
-            return 1;
-        }
+// Import Source Video
+function importVideo(file)
+{
+    // IMPORT VIDEO
+    app.project.importFiles(file.fsName, 0, sourceBin, 0);
+    
+    var timeLength = sourceBin.children[0].getOutPoint(0);
 
-        // CONVERT to seconds
-        arr[i][0] = parseInt(start[0]) * 60 + parseInt(start[1]);
-        arr[i][1] = parseInt(end[0]) * 60 + parseInt(end[1]);
-
-        //ERROR CHECK - start must be less than end
-        if (arr[i][0] >= arr[i][1])
-        {
-            log("Text file formatting error[5] on line " + (arr[i][2]));
-            return 1;
-        }
-        
+    if (maxTS.time > timeLength.seconds)
+    {
+        sourceBin.deleteBin();
+        alert("Timestamp Error on line " + maxTS.line + ":\nA timestamp does not exist in media");
+        return 1;
     }
 
     return 0;
 }
 
-// CREATES A NEW SEQUENCE BASED ON SET PRESET
-function createSequence()
+
+// CREATES SUBCLIPS and add to array
+function createSubclips(subclips)
 {
-    var name = "";
-    var count = 0;
-    
-    do
-    {
-        name = "seq" + (count < 10 ? 0:"") + count;
-        count++;
-        if (count > 100)
-            return 1;
-    } while(findItem(name, app.project.rootItem) != null)
-    
-    app.project.createNewSequence(name,"ID-" + name);
-
-    return name;
-}
-
-
-// CREATES SUBCLIPS AND ADDS THEM TO NEW SEQUENCE
-function createSubclips(timestamps, subclips)
-{
-    var root = app.project.rootItem;
-    var sourceBin = findItem("source", root);
-    var subclipBin = findItem("subclips", root);
-    
-    // ERROR CHECK - is there ONE video in the source bin
-    if (sourceBin.children.length == 0)
-    {
-        log("Please place a source video in the \"source\" bin.");
-        return 1;
-    }
-    if (sourceBin.children.length > 1)
-    {
-        log("There shouhld only be ONE (1) video in the \"source\" bin.");
-        return 1;
-    }
-
-    // FIND Subclips Bin
-    if (subclipBin == null)
-    {
-        subclipBin = root.createBin("subclips");
-    }
-
-    //  ERROR CHECK -  is subclip bin empty
-    if (subclipBin.children.length != 0)
-    {
-        log("Error: Subclip Bin should be Empty");
-        return 1;
-    }
-
     // Create Subclips from source video
     for (var i = 0; i < timestamps.length; i++)
     {
+        // Name of clip as seen in project panel
         var name = "clip" + (i < 100 ? 0:"") + (i < 10 ? 0:"") + i;
+
+        // Start and End times of timestamp
         var start = timestamps[i][0];
         var end = timestamps[i][1];
-    
-        sourceBin.children[0].createSubClip(name, start, end, 0, 1, 1);
+        
+        // CREATE clip
+        var clip = sourceBin.children[0].createSubClip(name, start, end, 0, 1, 1);
 
-        findItem(name, root).moveBin(subclipBin);
+        // MOVE clip to subclip bin
+        clip.moveBin(subclipsBin);
 
-        subclips.push(findItem(name,subclipBin));
+        // ADD clip to array of clips for inserting later
+        subclips.push(clip);
     }
 
     return 0;
 }
-
-
-function addClipsToSequence(clips, seqName)
-{
-    // ERROR - is subclips folder empty
-    if (clips.length < 1)
-    {
-        log("Error: no subclips found");
-        return 1;
-    }
-
-    // FIND SEQUENCE
-    var seq = findSequence(seqName);
-    if (seq == 1)
-    {
-        log("No sequence found");
-        return 1;
-    }
-
-    // OPEN SEQUENCE
-    if(!app.project.openSequence(seq.sequenceID))
-    {
-        log("Error: activating sequenece was unsuccessful");
-        return 1;
-    }
-
-    // ADD CLIPS TO SEQUENCE
-    for (var i = clips.length - 1; i >= 0; i--)
-    {
-        seq.videoTracks[0].insertClip(clips[i],0);
-    }
-    return 0;
-}
-
 
 
 //------------------------------------------------------------------------------
 // MISC FUNCTIONS
-
-// WRITES ALL ITEMS IN AN ARRAY INTO THE CONSOLE
-function writeAll(arr)
-{
-    for (var i = 0; i < arr.length; i++)
-    {
-        log(arr[i] + "\n");
-    }
-    
-}
-
 
 // RETURNS OBJECT OF ITEM IN A BIN ITEM - BUT NOT IN SUB-BINS
 function findItem(name, currentBin)
@@ -332,7 +238,6 @@ function findItem(name, currentBin)
         var child = currentBin.children[i];
         if (child.name.toLowerCase() == name.toLowerCase())
         {
-            //log(name + " FOUND!\n");
             return child;
         } 
     }
@@ -345,22 +250,43 @@ function isInt(value)
     return !isNaN(value) && parseInt(value) == value;
 }
 
-// PRINTS TO CONSOLE
-function log(str)
+// RETURNS user selected file after validation
+function fetchFile(formats, displayMessage)
 {
-    $.write(str + "\n");
+    alert(displayMessage); 
+    var file = File.openDialog(displayMessage, "");
+    
+    // ERROR CHECK - is file NULL?
+    if (file == null)
+    {
+        alert("File not selected");
+        return 1;
+    }
+
+    var i = file.displayName.lastIndexOf(".");
+    var formatName = file.displayName.substring(i, file.displayName.length);
+
+    // ERROR CHECK - is correct filetype?
+    if (formats.indexOf(formatName) == -1)
+    {
+        alert("Wrong file format.");
+        return 1;
+    }
+
+    return file;
 }
 
-// FINDS SEQUENCE
-function findSequence(name)
+// Creates a new BIN whilst checking for identical names. 
+function newBin(bin, name)
 {
-    var seqs = app.project.sequences
-    for (var i = 0; i < seqs.length; i++)
+    var count = 0;
+    var finalName = name;
+
+    while (findItem(finalName, bin) != null)
     {
-        if (seqs[i].name == name)
-        {
-            return seqs[i];
-        }
+        finalName = name + "-" + (count < 10 ? "0":"") + count;
+        count++;
     }
-    return 1;
+
+    return bin.createBin(finalName);
 }
